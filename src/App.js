89f19b4e95efc04905add25a0e9327e4adf07f18
dima dubinin <drakingin@gmail.com>
2025-05-08 22:21:01 +0300

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Chapter />} />
-
+        <Route path="/readchapter/:mangaName/chapter/:chapterNumber" element={<Chapter />} />
       </Routes>
       </BrowserRouter>
     </div>
